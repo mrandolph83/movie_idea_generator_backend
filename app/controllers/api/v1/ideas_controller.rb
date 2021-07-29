64516e98b43cdac2ad27c4adda1d8599b04b04a2
mrandolph83 <mrandolph83@gmail.com>
2021-07-29 16:39:20 -0400

    Ideas controller, index and create routes

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,3 +1,23 @@
 class Api::V1::IdeasController < ApplicationController
 
+def index
+    ideas = Idea.all 
+    render json: ideas
+end 
+
+def create
+    idea = Idea.new(idea_params)
+    if idea.save
+        render json: idea, status: :accepted
+    else
+        render joson: {errors: idea.errors.full_messages}, status: :unprocessible_entity
+    end
+end 
+
+private 
+def idea_params
+    params.require(:idea).permit(:character, :setup, :twist, :genre_id)
+end 
+
 end
+  
